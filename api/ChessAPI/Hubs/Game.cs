6d69d87fdd33671bed92761cf43dde4acbacc5fd
Marie Halloran <mhalloran@microsoft.com>
2026-06-30 15:06:30 -0400

    Update type on grain

--- api/ChessAPI/Hubs/Game.cs
+++ api/ChessAPI/Hubs/Game.cs
@@ -15,7 +15,7 @@ namespace ChessAPI.Hubs
         {
             Console.WriteLine($"move received for game {gameId}");
             Console.WriteLine($"move received for game {snapshot}");
-            var grain = _grainFactory.GetGrain<IGameGrain>(gameId);
+            IGameGrain grain = _grainFactory.GetGrain<IGameGrain>(gameId);
             await grain.Update(snapshot.ToString());
             await Clients.OthersInGroup(gameId).SendAsync("ReceiveMove", snapshot);
             //Instead of just sending moves back to client right away, will send to orleans
@@ -23,24 +23,23 @@ namespace ChessAPI.Hubs
 
         public async Task JoinGame(string gameId)
         {
-
+            //Just a listener right now
             await Groups.AddToGroupAsync(Context.ConnectionId, gameId);
             Console.WriteLine($"game joined: {gameId}");
         }
         public async Task<string> CreateGame()
         {
-            var gameId = Guid.NewGuid().ToString();
-            var grain = _grainFactory.GetGrain<IGameGrain>(gameId);
+            string gameId = Guid.NewGuid().ToString();
+            IGameGrain grain = _grainFactory.GetGrain<IGameGrain>(gameId);
             await grain.Create();
             await Groups.AddToGroupAsync(Context.ConnectionId, gameId);
-            Console.WriteLine($"game created: {gameId}");
             //Create a game grain in orleans and store the gameId there so that it can be tracked
             return gameId;
         }
 
         public async Task CloseGame(string gameId)
         {
-            var grain = _grainFactory.GetGrain<IGameGrain>(gameId);
+            IGameGrain grain = _grainFactory.GetGrain<IGameGrain>(gameId);
             await grain.Close();
             await Clients.OthersInGroup(gameId).SendAsync("GameClosed", gameId);
             //Instead of just sending moves back to client right away, will send to orleans
