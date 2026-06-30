0a6d73e662b6ff3f7e5cfafcdf1b45fdbfbfffa8
Marie Halloran <mhalloran@microsoft.com>
2026-06-30 14:16:59 -0400

    profile store must match what's at the root

--- api/ChessAPI/Grains/Game.cs
+++ api/ChessAPI/Grains/Game.cs
@@ -4,7 +4,7 @@ public class GameGrain : Grain, IGameGrain
 {
     private readonly IPersistentState<GameState> _state;
 
-    public GameGrain([PersistentState("game", "cosmos")] IPersistentState<GameState> state)
+    public GameGrain([PersistentState("game", "profileStore")] IPersistentState<GameState> state)
         => _state = state;
 
     public async Task Create()
